f5340649c72317d360dd9703034e8aaefddc8cd7
desiree <desireegarcia@mini.local>
2021-10-18 15:11:55 -0600

    started code for LogIn page

---
+++ African Marketplace/src/Components/LogIn.js
@@ -0,0 +1,35 @@
+Login.js
+import React from 'react';
+
+export default function LogIn() {
+
+    return {
+        <form>
+            <div>
+                <div>
+                <label>Username
+                    <input 
+                    type="text"
+                    name="username"
+                    id="username"
+                    value={values.username}
+                    onChange={onChange}
+                    />
+                </label>
+                </div>
+                <div>
+                <label>
+                    <input 
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={values.password}
+                    onChange={onChange}
+                    />
+                </label>
+                <button disabled={disabled}>Login</button>
+                </div>
+            
+            </div>
+    }
+}
